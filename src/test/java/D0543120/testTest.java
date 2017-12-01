abfd2c43d95eb8551394ad606c2027a5bd5f5709
a74281124 <a74281124@gmail.com>
2017-12-01 20:31:12 +0800

    test commit

---
+++ src/test/java/D0543120/testTest.java
@@ -0,0 +1,18 @@
+package D0543120;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testTest {
+    @Test
+    public void hello() throws Exception {
+        test sayhello = new test();
+        String result = sayhello.Hellow(1);
+        Assert.assertEquals("Hello 1",result);
+
+
+    }
+
+}
